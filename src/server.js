74d40ae7048402578a7962a5635a8ef479e27de6
Paulo Luan <pauloluan1409@gmail.com>
2026-02-12 01:16:28 -0300

    feat: add cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
-import express from 'express';
-import routes from './routes.js';
+import express from "express";
+import cors from "cors";
+import routes from "./routes.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
 app.listen(3001, () => {
-  console.log('Bot Manager rodando na porta 3001');
+  console.log("Bot Manager rodando na porta 3001");
 });
